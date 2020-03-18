c556ce9aad50affac8fc577c6e22b9ed059bfe9d
Eugene Lee <eugenelukelee@gmail.com>
2020-03-17 20:04:37 -0700

    fills form to edit and changed frontend edit

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -374,19 +374,22 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
     form = ArtistForm()
-    artist = {
-        "id": 4,
-        "name": "Guns N Petals",
-        "genres": ["Rock n Roll"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "326-123-5000",
-        "website": "https://www.gunsnpetalsband.com",
-        "facebook_link": "https://www.facebook.com/GunsNPetals",
-        "seeking_venue": True,
-        "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-        "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-    }
+    artist = Artist.query.get(artist_id)
+
+    if artist is not None:
+        form.name.data = artist.name,
+        form.genres.data = artist.genres,
+        form.city.data = artist.city,
+        form.state.data = artist.state,
+        form.phone.data = artist.phone,
+        form.website.data = artist.website,
+        form.facebook_link.data = artist.facebook_link,
+        form.seeking_venue.data = artist.seeking_venue,
+        form.seeking_description.data = artist.seeking_description,
+        form.image_link = artist.image_link
+    else:
+        flash('Artist ID does not exist')
+        return render_template('pages/home.html')
     # populate form with fields from artist with ID <artist_id>
     return render_template('forms/edit_artist.html', form=form, artist=artist)
 
@@ -402,21 +405,24 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
     form = VenueForm()
-    venue = {
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    }
-    # TODO: populate form with values from venue with ID <venue_id>
+    venue = Venue.query.get(venue_id)
+
+    if venue is not None:
+        form.name.data = venue.name,
+        form.genres.data = venue.genres,
+        form.address.data = venue.address,
+        form.city.data = venue.city,
+        form.state.data = venue.state,
+        form.phone.data = venue.phone,
+        form.website.data = venue.website,
+        form.facebook_link.data = venue.facebook_link,
+        form.seeking_talent.data = venue.seeking_talent,
+        form.seeking_description.data = venue.seeking_description,
+        form.image_link = venue.image_link
+    else:
+        flash('Venue ID does not exist')
+        return render_template('pages/home.html')
+    # populate form with values from venue with ID <venue_id>
     return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 
