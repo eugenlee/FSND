e227cbd83de7808aa5d56d5bd2e4bfe26e6e3107
Eugene <eugenelukelee@gmail.com>
2020-04-17 00:16:40 -0700

    update npm, change categories {}, delete endoint

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -49,12 +49,11 @@ def create_app(test_config=None):
 
     return jsonify({
       'success': True,
-      'categories': [category.format() for category in categories],
+      'categories': {category.id: category.type for category in categories},
       'total_categories':len(Category.query.all())
     })
 
   '''
-  @TODO: 
   Create an endpoint to handle GET requests for questions, 
   including pagination (every 10 questions). 
   This endpoint should return a list of questions, 
@@ -78,17 +77,36 @@ def create_app(test_config=None):
     return jsonify({
       'success': True,
       'questions': current_questions,
-      'total_questions': len(current_questions),
-      'categories': [category.format() for category in categories]
+      'total_questions': len(Question.query.all()),
+      'categories': {category.id: category.type for category in categories}
     })
   '''
-  @TODO: 
   Create an endpoint to DELETE question using a question ID. 
 
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    try:
+      question = Question.query.filter(Question.id == question_id).one_or_none()
 
+      if question is None:
+        abort(404)
+
+      question.delete()
+      selection = Question.query.order_by(Question.id).all()
+      current_questions = paginate_questions(request,selection)
+
+      return jsonify({
+        'success': True,
+        'deleted': question_id,
+        'questions': current_questions,
+        'total_questions': len(Question.query.all())
+    })
+
+    except:
+      abort(422)
   '''
   @TODO: 
   Create an endpoint to POST a new question, 
